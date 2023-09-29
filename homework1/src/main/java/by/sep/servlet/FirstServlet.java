0fd3a764ba233eed383b6ec1d667573b0d901026
sephir <a.bulgarin@gmail.com>
2023-09-29 23:52:11 +0300

    test

---
+++ homework1/src/main/java/by/sep/servlet/FirstServlet.java
@@ -0,0 +1,28 @@
+package by.sep.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "FirstServlet", urlPatterns = "/firstservlet")
+public class FirstServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("<html><head><title>First Servlet</title></head>");
+        out.println("<body><h1>This is First Servlet</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
