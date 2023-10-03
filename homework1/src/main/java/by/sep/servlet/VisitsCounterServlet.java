0f6dcc218401ca883637e11cc7d39798120b5aa1
sephir <a.bulgarin@gmail.com>
2023-10-03 23:38:16 +0300

    homework1

--- homework1/src/main/java/by/sep/servlet/VisitsCounterServlet.java
+++ homework1/src/main/java/by/sep/servlet/VisitsCounterServlet.java
@@ -2,20 +2,22 @@ package by.sep.servlet;
 
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.concurrent.atomic.AtomicInteger;
-
 
 @WebServlet(name = "VisitsCounter", urlPatterns = "/visitscounter.html")
 public class VisitsCounterServlet extends HttpServlet {
     private AtomicInteger visitsCounter;
-    File counterFile = new File("/d/work/homework/homework1/" +
-            "src/main/java/by/sep/counter.txt");
+    File counterFile = new File("/d/work/homework/homework1/" + "src/main/java/by/sep/counter.txt");
 
     @Override
     public void init(ServletConfig config) throws ServletException {
@@ -35,16 +37,18 @@ public class VisitsCounterServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        visitsCounter.incrementAndGet();
+        String text = "Total visits - " + visitsCounter.incrementAndGet();
         try (FileWriter fileWriter = new FileWriter(counterFile)) {
             fileWriter.write(String.valueOf(visitsCounter));
             fileWriter.flush();
         }
-        String title = "Total visits - ";
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        out.println("<html><head><title>VisitsCounterServlet</title></head>");
-        out.println("<body><h1>" + title + visitsCounter + "</h1>");
-        out.println("</body></html>");
+        resp.setContentType("image/jpeg");
+        BufferedImage image = new BufferedImage(500, 200, BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = image.createGraphics();
+        graphics.setFont(new Font("Serif", Font.ITALIC, 48));
+        graphics.setColor(Color.GREEN);
+        graphics.drawString(text, 100, 100);
+        ServletOutputStream servletOutputStream = resp.getOutputStream();
+        ImageIO.write(image, "jpeg", servletOutputStream);
     }
 }
