fe5bb01231176b9b06b4335a1f5210d9ef7963eb
abulgarin <a.bulgarin@gmail.com>
2023-12-07 00:36:11 +0300

    homework5 first commit  added task2

---
+++ homework5/src/main/java/by/sep/task2/Student.java
@@ -0,0 +1,58 @@
+package by.sep.task2;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import java.util.Map;
+
+public class Student implements InitializingBean, DisposableBean {
+    private String firstName;
+    private String lastName;
+    private Map<String, String> marks;
+
+    public static Student getInstance() {
+        return new Student();
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        System.out.println("Student initialization");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Student destruction");
+    }
+
+    public void defaultInit() {
+        System.out.println("Default student initialization");
+    }
+
+    public void defaultDestroy() {
+        System.out.println("Default student destruction");
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Map<String, String> getMarks() {
+        return marks;
+    }
+
+    public void setMarks(Map<String, String> marks) {
+        this.marks = marks;
+    }
+}
